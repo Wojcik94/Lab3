e0572a7def5715ce0faade689edacdd923939e10
Wojcik94 <wojcik94@interia.pl>
2017-04-05 21:46:42 +0200

    add new files

---
+++ SimEngine.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Wojcik94 on 2017-04-04.
+ */
+public class SimEngine {
+}
---
+++ SpringApplet.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Wojcik94 on 2017-04-04.
+ */
+import javax.swing.*;
+import java.awt.*;
+
+public class SpringApplet extends JApplet {
+    public void init(){
+
+    }
+
+    public void paint(Graphics g){
+
+    }
+}
